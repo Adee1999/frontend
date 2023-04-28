340059a41c68d405fb5997f8d298ddfd680fc63e
Adee1999 <126112423+Adee1999@users.noreply.github.com>
2023-04-28 16:08:09 +0600

    Update personalinfo.js

--- pages/Forms/personalinfo.js
+++ pages/Forms/personalinfo.js
@@ -4,18 +4,7 @@ function PersonalInfo({ formData, setFormData }) {
   return (
     <>
      
-      <FormInputGroup
-        label="Whatsapp номер"
-        type="text"
-        value={formData.lastName}
-        onChange={(e) => setFormData({ ...formData, lastName: e.target.value })}
-      />
-      <FormInputGroup
-        label="email"
-        type="text"
-        value={formData.userName}
-        onChange={(e) => setFormData({ ...formData, userName: e.target.value })}
-      />
+     
     </>
   );
 }
