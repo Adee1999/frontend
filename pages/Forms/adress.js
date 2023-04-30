008ab81b40c0d84701fe7acbeeef6f8eae8f794b
Adee1999 <126112423+Adee1999@users.noreply.github.com>
2023-04-30 13:35:02 +0600

    Update adress.js

--- pages/Forms/adress.js
+++ pages/Forms/adress.js
@@ -4,8 +4,24 @@ import FormInputGroup from "../../components/form/forminputgroup";
 function Address({ formData, setFormData }) {
   return (
     <>
-      
-      
+      <FormInputGroup
+        type="text"
+        label="Город, Район"
+        value={formData.street}
+        onChange={(e) => setFormData({ ...formData, street: e.target.value })}
+      />{" "}
+      <FormInputGroup
+        type="text"
+        label="Улица дом"
+        value={formData.city}
+        onChange={(e) => setFormData({ ...formData, city: e.target.value })}
+      />
+      <FormInputGroup
+        type="text"
+        label="Почта индекс"
+        value={formData.password}
+        onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+      />
     </>
   );
 }
