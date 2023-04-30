4405bd3d8d22af997f00512597666a0a928c1d22
Adee1999 <126112423+Adee1999@users.noreply.github.com>
2023-04-30 14:04:15 +0600

    Update signup.jsx

--- pages/Forms/signup.jsx
+++ pages/Forms/signup.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import FormInputGroup from "../../components/form/forminputgroup";
 function SingUp() {
   return (
-    <>
+    <form>
       
-    </>
+    </form>
   );
 }
 
