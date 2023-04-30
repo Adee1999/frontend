e88135b43bf72190a371743377405673572b8ce4
Adee1999 <126112423+Adee1999@users.noreply.github.com>
2023-04-30 14:15:16 +0600

    Update adress.jsx

--- pages/Forms/adress.jsx
+++ pages/Forms/adress.jsx
@@ -5,19 +5,16 @@ function Address({ formData, setFormData }) {
   return (
     <form>
       <FormInputGroup
-        type="text"
         label="Город, Район"
         value={formData.street}
         onChange={(e) => setFormData({ ...formData, street: e.target.value })}
       />{" "}
       <FormInputGroup
-        type="text"
         label="Улица дом"
         value={formData.city}
         onChange={(e) => setFormData({ ...formData, city: e.target.value })}
       />
       <FormInputGroup
-        type="text"
         label="Почта индекс"
         value={formData.password}
         onChange={(e) => setFormData({ ...formData, password: e.target.value })}
